f91a189cdb978546bc83a507f6ead6392c0b4fb5
yacoub cherouali <cheroualiy@gmail.com>
2025-03-06 20:48:51 +0100

    mariadb configurating

---
+++ src/requirements/mariadb/conf/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mariadb-install-db --user=mysql --basedir=/usr --datadir="/var/lib/mysql"
+fi
+
+sed -i "s|bind-address\s*=\s*127.0.0.1|bind-address = 0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mysqld_safe --datadir="$/var/lib/mysql" &
+sleep 10
+
+
+echo "MariaDB configurating process.."
+
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ADMIN_USER}'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' WITH GRANT OPTION;"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+wait
